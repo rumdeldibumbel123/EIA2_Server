803661eb48d13ef352d57a6dd9fb0a95c7ec737e
rumdeldibumbel123 <32680802+rumdeldibumbel123@users.noreply.github.com>
2018-01-24 17:48:47 +0100

    Add files via upload

---
+++ ServerTest.js
@@ -0,0 +1,46 @@
+"use strict";
+// Node-Http-Modul importieren
+var Http = require("http");
+// Node-Url-Modul importieren
+var Url = require("url");
+var ServerTest;
+(function (ServerTest) {
+    // Port vom Process-Objekt erfragen 
+    var port = process.env.PORT;
+    // Port nicht definiert -> lokale Maschine, Port selbst definieren
+    if (port == undefined)
+        port = 5000;
+    // Server-Objekt kreieren
+    var server = Http.createServer();
+    // Event-Handler installieren
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
+    // Auf dem Port horchen
+    server.listen(port);
+    // Listening-Event: Rückmeldung wenn horchen läuft
+    function handleListen() {
+        console.log("Server listening on port " + port);
+    }
+    // Request-Event: Verarbeiten der Request und erstellen der Response
+    function handleRequest(_request, _response) {
+        console.log("Ich höre Stimmmmen!!");
+        // Header: Antwort kommt im HTML-Format mit uft-8
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        // Header: ?
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        // Response-Body
+        _response.write("Danke für Ihre Bestellung<br>");
+        _response.write("");
+        _response.write("");
+        _response.write("");
+        //_response.write("Headers: " + _request.headers + "<br>");
+        // ?
+        var query = Url.parse(_request.url, true).query;
+        // ?
+        for (var key in query)
+            _response.write("<p>" + key + ": " + query[key] + "</p>");
+        // Antwort abschließen und abschicken
+        _response.end();
+    }
+})(ServerTest || (ServerTest = {}));
+//# sourceMappingURL=ServerTest.js.map
---
+++ ServerTest.ts
@@ -0,0 +1,55 @@
+// Node-Http-Modul importieren
+import * as Http from "http";
+// Node-Url-Modul importieren
+import * as Url from "url";
+
+namespace ServerTest {
+    // Neuer Datentyp AssocStringString: homogenes, assoziatives Array.
+    interface AssocStringString {
+        [key: string]: string;
+    }
+    
+    // Port vom Process-Objekt erfragen 
+    let port: number = process.env.PORT;
+    // Port nicht definiert -> lokale Maschine, Port selbst definieren
+    if (port == undefined)
+        port = 5000;
+    
+    // Server-Objekt kreieren
+    let server: Http.Server = Http.createServer();
+    // Event-Handler installieren
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
+    // Auf dem Port horchen
+    server.listen(port);
+
+    // Listening-Event: Rückmeldung wenn horchen läuft
+    function handleListen(): void {
+        console.log("Server listening on port " + port);
+    }
+
+    // Request-Event: Verarbeiten der Request und erstellen der Response
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Ich höre Stimmmmen!!");
+        // Header: Antwort kommt im HTML-Format mit uft-8
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        // Header: ?
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        
+        // Response-Body
+        _response.write("Danke für Ihre Bestellung<br>");
+        _response.write("");
+        _response.write("");
+        _response.write("");
+        //_response.write("Headers: " + _request.headers + "<br>");
+
+        // ?
+        let query: AssocStringString = Url.parse(_request.url, true).query;
+        // ?
+        for (let key in query)
+            _response.write("<p>"+key + ": " + query[key]+"</p>");
+        
+        // Antwort abschließen und abschicken
+        _response.end();
+    }
+}
